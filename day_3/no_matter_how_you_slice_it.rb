70e4b82715fbd4f6e0e729c5a2a29cbdfad7793e
dezande <vincent.corniere@jobteaser.com>
2019-02-12 17:17:55 +0100

    Initialize the claim

--- day_3/no_matter_how_you_slice_it.rb
+++ day_3/no_matter_how_you_slice_it.rb
@@ -2,6 +2,36 @@ class NoMatterHowYouSliceIt
   attr_reader :claims
 
   def initialize(inputs)
-    @claims = inputs
+    @claims = inputs.map do |input|
+      {
+        id: id(input),
+        left_edge: left_edge(input),
+        top_edge: top_edge(input),
+        wide: wide(input),
+        tall: tall(input)
+      }
+    end
+  end
+
+  private
+
+  def id(input)
+    input.scan(/#\d+/).first[1..-1].to_i
+  end
+
+  def left_edge(input)
+    input.scan(/\d+,/).first[0...-1].to_i
+  end
+
+  def top_edge(input)
+    input.scan(/,\d+/).first[1..-1].to_i
+  end
+
+  def wide(input)
+    input.scan(/\d+x/).first[0...-1].to_i
+  end
+
+  def tall(input)
+    input.scan(/x\d+/).first[1..-1].to_i
   end
 end
--- day_3/no_matter_how_you_slice_it_test.rb
+++ day_3/no_matter_how_you_slice_it_test.rb
@@ -6,14 +6,27 @@ require_relative 'no_matter_how_you_slice_it'
 class NoMatterHowYouSliceItTest < Minitest::Test
   def test_read_a_claim
     claims = ['#123 @ 3,2: 5x4']
-    result = NoMatterHowYouSliceIt.new(claims),
+    result = NoMatterHowYouSliceIt.new(claims)
     assert_equal(
-      result.claims,
-      id: '123',
+      result.claims.first,
+      id: 123,
       left_edge: 3,
       top_edge: 2,
-      wides: 5,
+      wide: 5,
       tall: 4
+    )
+  end
+
+  def test_read_a_other_claim
+    claims = ['#1 @ 50,30: 64x23']
+    result = NoMatterHowYouSliceIt.new(claims)
+    assert_equal(
+      result.claims.first,
+      id: 1,
+      left_edge: 50,
+      top_edge: 30,
+      wide: 64,
+      tall: 23
     )
   end
 end

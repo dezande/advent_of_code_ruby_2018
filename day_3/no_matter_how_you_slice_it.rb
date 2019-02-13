66a7cf7c90de1dd108e6449dcdfb426be0947215
dezande <vincent.corniere@jobteaser.com>
2019-02-13 18:47:05 +0100

    End second part

--- day_3/no_matter_how_you_slice_it.rb
+++ day_3/no_matter_how_you_slice_it.rb
@@ -1,5 +1,7 @@
+require 'set'
+
 class NoMatterHowYouSliceIt
-  attr_reader :claims
+  attr_reader :claims, :candidates, :square
 
   def initialize(inputs)
     @claims = inputs.lines.inject({}) do |memo, input|
@@ -12,39 +14,36 @@ class NoMatterHowYouSliceIt
         }
       )
     end
+    @candidates = Set.new(claims.keys)
+    @square = initialize_square
   end
 
   def square_inches
     square
       .flatten
-      .select { |cell| cell > 1 }
+      .select { |cell| cell.count > 1 }
       .count
   end
 
   def claim_don_t_overlap
-    claims.each do |id, claim|
-      return id unless in_the_square?(claim)
-    end
+    candidates.to_a.first
   end
 
   private
 
-  def square
+  def initialize_square
     claims
-      .each_with_object(Array.new(1000) { Array.new(1000, 0) }) do |claim, memo|
+      .each_with_object(Array.new(1000) { Array.new(1000) { Set.new } }) do |claim, memo|
         claim.last[:wide].times do |x|
           claim.last[:tall].times do |y|
-            memo[x + claim.last[:left_edge]][y + claim.last[:top_edge]] += 1
+            memo[x + claim.last[:left_edge]][y + claim.last[:top_edge]].add claim.first
+
+            if memo[x + claim.last[:left_edge]][y + claim.last[:top_edge]].count > 1 
+              @candidates -= memo[x + claim.last[:left_edge]][y + claim.last[:top_edge]]
+            end
           end
         end
       end
-  end
-
-  def in_the_square?(wide:, tall:, left_edge:, top_edge:)
-    square[left_edge][top_edge] > 1 ||
-      square[left_edge][tall + top_edge - 1] > 1 ||
-      square[wide + left_edge - 1][top_edge] > 1 ||
-      square[wide + left_edge - 1][tall + top_edge - 1] > 1
   end
 
   def find_id(input)
